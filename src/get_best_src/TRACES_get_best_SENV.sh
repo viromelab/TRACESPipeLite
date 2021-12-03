26de117389e887c5da4eccd552e4687def24aba5
pratas <6882743+pratas@users.noreply.github.com>
2021-12-03 16:48:58 +0000

    VDB and others

--- src/get_best_src/TRACES_get_best_SENV.sh
+++ src/get_best_src/TRACES_get_best_SENV.sh
@@ -3,7 +3,7 @@
 RESULT=`cat top-metagenomics.csv \
 | grep -a -e "SEN-V" -e "SEN virus" \
 | grep -a -e "omplete genome" -e "omplete_genome" \
-| awk '{ if($3 > 0 && $2 > 2000 && $2 < 10000) print $3"\t"$4; }' \
+| awk '{ if($3 > 0 && $2 > 1500 && $2 < 10000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \
 | sed "s/NC\_/NC-/" \
