14a68ebe3df96c66c018253183025710eafdf9f9
pratas <6882743+pratas@users.noreply.github.com>
2021-12-07 15:03:46 +0000

    fix MT

--- src/get_best_src/TRACES_get_best_MT.sh
+++ src/get_best_src/TRACES_get_best_MT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 RESULT=`cat top-metagenomics.csv \
-| grep -a -e "NC_012920.1" \ 
-| awk '{ if($3 > 0 && $2 > 14000 && $2 < 17000) print $3"\t"$4; }' \
+| grep -a "NC_012920.1" \ 
+| awk '{ if($3 > 0 && $2 > 14000 && $2 < 18000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \
 | sed "s/NC\_/NC-/" \
